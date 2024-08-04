a95195869e565eb3efb6652fab23f6ee4b1eb9aa
Dredipatadija <andrearmra@gmail.com>
2024-08-04 14:52:47 +0200

    upload parse 100% ok

--- hooks.c
+++ hooks.c
@@ -11,14 +11,17 @@ static void	ft_exit(t_map **map, char c)
 	}
 }
 
-static int	ft_move(t_map **map)
+static int	ft_move(t_map **map, int new_y, int new_x)
 {
 	char	c;
 
-	c = (*map)->map[(*map)->player.y][(*map)->player.x];
+	c = (*map)->map[new_y][new_x];
 	if (c == '0')
 	{
 		(*map)->map[(*map)->player.y][(*map)->player.x] = '0';
+		(*map)->player.y = new_y;
+        (*map)->player.x = new_x;
+        (*map)->map[new_y][new_x] = 'P';
 		(*map)->player.steps++;
 		ft_printf("Steps: %i Coins: %i\n", (*map)->player.steps,
 				(*map)->player.coin);
@@ -27,6 +30,9 @@ static int	ft_move(t_map **map)
 	if (c == 'C')
 	{
 		(*map)->map[(*map)->player.y][(*map)->player.x] = '0';
+		(*map)->player.y = new_y;
+        (*map)->player.x = new_x;
+        (*map)->map[new_y][new_x] = 'P';
 		(*map)->player.steps++;
 		(*map)->player.coin++;
 		ft_printf("Steps: %i Coins: %i\n", (*map)->player.steps,
@@ -39,34 +45,42 @@ static int	ft_move(t_map **map)
 	return (0); 
 }
 
-static void ft_move_y(t_map **map, int true, int y)
+static void ft_move_y(t_map **map, int y)
 {
-	if (true == 1)
-	{
-		(*map)->player.y = (*map)->player.y + y;
-		(*map)->map[(*map)->player.y][(*map)->player.x] = 'P';
-	}
+	int new_y;
+    int new_x;
+
+	new_y = (*map)->player.y + y;
+	new_x = (*map)->player.x;
+    if (ft_move(map, new_y, new_x))
+    {
+        (*map)->player.y = new_y;
+    }
 }
 
-static void	ft_move_x(t_map **map, int true, int x)
+static void	ft_move_x(t_map **map, int x)
 {
-	if (true == 1)
-	{
-		(*map)->player.x = (*map)->player.x + x;
-		(*map)->map[(*map)->player.y][(*map)->player.x] = 'P';
-	}
+	int new_y;
+    int new_x;
+
+	new_y = (*map)->player.y;
+	new_x = (*map)->player.x + x;
+    if (ft_move(map, new_y, new_x))
+    {
+        (*map)->player.x = new_x;
+    }
 }
 
 static void	ft_which_hook(int keycode, t_map **map)
 {
 	if (keycode == UP || keycode == W)
-		ft_move_y(map, ft_move(map), -1);
-	if (keycode == DOWN || keycode == S)
-		ft_move_y(map, ft_move(map), 1);
-	if (keycode == LEFT || keycode == A)
-		ft_move_x(map, ft_move(map), -1);
-	if (keycode == RIGHT || keycode == D)
-		ft_move_x(map, ft_move(map), 1);
+        ft_move_y(map, -1);
+    if (keycode == DOWN || keycode == S)
+        ft_move_y(map, 1);
+    if (keycode == LEFT || keycode == A)
+        ft_move_x(map, -1);
+    if (keycode == RIGHT || keycode == D)
+        ft_move_x(map, 1);
 }
 
 int	ft_hook(int keycode, t_map **map)
